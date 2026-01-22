a36348148c1a6be00d207fca99027bb445d369c0
Tijana <tjnmlsvljvc@gmail.com>
2026-01-22 20:02:00 +0100

    fix: update authService after merge

--- src/features/authentication/services/authService.js
+++ src/features/authentication/services/authService.js
@@ -1,14 +1,11 @@
 import Api from "../../../core/services/api";
 
-<<<<<<< HEAD
 export const loginUser = async (userData) => {
     const response = await Api.post("/api/Users/login", userData);
     return response.data;
 };
-=======
 
 export async function registerUser(user) {
   const response = await Api.post("/api/Users/register", user);
   return response.data;
 }
->>>>>>> feature/user-registration
